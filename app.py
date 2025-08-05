8d5e918605f5c17d57a45c58ef00526a4c69d163
Gnana_Jothi <Gnana_Jothi@gmail.com>
2025-08-05 06:53:05 +0000

    final commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello from Jenkins CI/CD Pipeline with Docker and Flask!"
+    return "🚀 Hello from GitHub Actions CI/CD with Docker and Flask!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
